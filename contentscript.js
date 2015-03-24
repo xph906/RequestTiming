559e10d6321ff87d1259744ffde8b0696868899e
Xiang Pan <xiangpan2011@gmail.com>
2015-03-24 18:01:59 -0500

    content script

---
+++ contentscript.js
@@ -0,0 +1,34 @@
+// Content script -- contentscript.js
+
+var backgroundPageConnection = chrome.runtime.connect({name: "content-page"});
+backgroundPageConnection.postMessage({name:"helloMsg",info:"Hello Chrome, this is content script"});
+var DOMContentLoadedTime = 0;
+var loadTime = 0;
+
+var DOMContentLoadedListener = function(event){
+	var currentTime = new Date().getTime();
+	console.log("DOMContentLoadedEvent:"+currentTime);
+	DOMContentLoadedTime = currentTime;
+	var url = document.URL;
+	backgroundPageConnection.postMessage({name:"eventTimeMsg",eventName:"DOMContentLoaded",time:String(currentTime),url:url});
+	//alert("DOMContentLoadedEvent:"+url);
+}
+
+var LoadListener = function(event){
+	var currentTime = new Date().getTime();
+	var url = document.URL;
+	console.log("LoadEvent:"+currentTime);
+	loadTime = currentTime;
+	backgroundPageConnection.postMessage({name:"eventTimeMsg",eventName:"load",time:String(loadTime),url:url});
+	//alert("LoadEvent:"+url);
+}
+
+/*
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+	
+});
+*/
+
+window.addEventListener("DOMContentLoaded",DOMContentLoadedListener);
+window.addEventListener("load",LoadListener);
