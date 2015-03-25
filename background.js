de9d087ed69a7e32a719791d513539730d85cb19
xph906 <xiangpan2011@u.northwestern.edu>
2015-03-25 11:54:05 -0500

    ddd

--- background.js
+++ background.js
@@ -9,6 +9,41 @@ function URLElements(){
 var elements = new URLElements();
 var urlTable = {}
 
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+/***********************************************
+ URLClass
+ ***********************************************/
+function URLClass(url){
+    this.url = url.toLowerCase();
+    this.deadEnd = false;
+    this.waitForReceivingTime = false;
+    this.setDeadEnd();
+    this.setWaitForReceivingTime();
+}
+URLClass.prototype.setDeadEnd() = function(){
+    if( this.url.endsWith(".jpg") || this.url.endsWith(".png") ||
+        this.url.endsWith(".jpeg")|| this.url.endsWith(".swf") ||
+        this.url.endsWith(".gif") )
+        this.deadEnd = true;
+    else
+        this.deadEnd = false;
+}
+URLClass.prototype.setWaitForReceivingTime() = function(){
+    if( this.url.endsWith(".jpg") || this.url.endsWith(".png") ||
+        this.url.endsWith(".jpeg")|| this.url.endsWith(".swf") ||
+        this.url.endsWith(".gif") || this.url.endsWith(".js"))
+        this.waitForReceivingTime = true;
+    else
+        this.waitForReceivingTime = false;
+}
+URLClass.prototype.toString() = function(){
+    return "URL:"+this.url+" [DEADEND:"+this.deadEnd+"] [WAITFORRT:"+this.waitForReceivingTime+"]";
+}
+
+
 function iterateProperties(obj){
     bg.console.log("Object Properties:");
     for(var item in obj){
@@ -32,6 +67,7 @@ var devToolsListener = function(message, sender, sendResponse) {
     if(message.name == "requestMsg"){
         var request = JSON.parse(message.request);
         //bg.console.log("[devtools] receive request msg "+message.request);
+        request.url = new URLClass(request.url);
         elements.requests.push(request);
     }
     else if(message.name == "helloMsg"){
@@ -62,51 +98,60 @@ var contentScriptListener = function(message, sender, sendResponse) {
             //bg.console.log("onload event gets fired: received:"+elements.requests.length+" requests");
         }
         else if(message.eventName=="load"){
-            var url = message.url;
+            var firstURL = message.url;
             elements.loadTime = parseInt(message.time);
             bg.console.log("onload event gets fired: received:"+elements.requests.length+" requests");
             if(elements.requests.length > 0){
-                /*  FIXME: Note that the URL here is not accurate 
-                    because it is not the original URL if redirecion happens */
-                bg.console.log("  first url:"+url);
+                /* Keep the requests belonging to this page 
+                 * FIXME: Note that the URL here is not accurate 
+                 * because it is not the original URL if redirecion happens 
+                 */
+                bg.console.log("  first url:"+firstURL);
                 for(var index=0; index<elements.requests.length; index++){
-                    if(elements.requests[index].url == url){
+                    if(elements.requests[index].url.url == firstURL){
                         bg.console.log("  "+index+" request is the url");
                         elements.requests = elements.requests.slice(index);
                         break;
                     }
                 }
-                console.log("  "+elements.requests.length+" ");
-                urlTable[url] = elements;
+                console.log("keep "+elements.requests.length+" effective requests");
+                urlTable[firstURL] = elements;
 
-                /* sort the requests based on start time and end time */
+                /* Sort the requests based on start time and end time */
                 for(var i in elements.requests){
-                    //bg.console.log(elements.requests[i].url+": totaltime:"+elements.requests[i].totalTime);
-                    endTime = elements.requests[i].startTime + elements.requests[i].totalTime;
-                    elements.requests[i].endTime = endTime;
+                    bg.console.log(elements.requests[i].url.toString()+" Totaltime:"+elements.requests[i].totalTime);
+                    var url = elements.requests[i].url;
+                    if(url.waitForReceivingTime){
+                        endTime = elements.requests[i].startTime + elements.requests[i].totalTime;
+                    }
+                    else{
+                        endTime = elements.requests[i].startTime + elements.requests[i].totalTime -
+                                elements.requests[i].receiveTime;
+                    }
+                    elements.requests[i].endTime = endTime;         
+                    //bg.console.log("receiveTime:"+elements.requests[i].receiveTime+" url:"+elements.requests[i].url);
                 }
                 var sortedRequestsOnStartTime = elements.requests.slice();
                 var sortedRequestsOnEndTime = elements.requests.slice();
-                console.log(elements.requests.length+" sortedRequestsOnStartTime len:"+sortedRequestsOnStartTime.length + 
-                            " sortedRequestsOnEndTime len:"+sortedRequestsOnEndTime.length);
                 sortedRequestsOnStartTime.sort(compareRequestStartTime);
                 sortedRequestsOnEndTime.sort(compareRequestEndTime);
                 
-                /* create a dict to map a request's start time and end time */
+                /* Create a dict to map a request's start time and end time 
+                 * url => (startTimeList index, endTimeList index)
+                 */
                 var urlDict = {};
                 for(var i in sortedRequestsOnStartTime){
-                    var url1 = sortedRequestsOnStartTime[i].url;
-                    urlDict[url1] = [i];
-                    urlDict[url1]
+                    var urlOnStartArr = sortedRequestsOnStartTime[i].url.url;
+                    urlDict[urlOnStartArr] = [i];
                     for(var j in sortedRequestsOnEndTime){
-                        var url2 = sortedRequestsOnEndTime[j].url;
-                        if(url1 == url2){
-                            urlDict[url1].push(j);
+                        var urlOnEndArr = sortedRequestsOnEndTime[j].url.url;
+                        if(urlOnStartArr == urlOnEndArr){
+                            urlDict[urlOnStartArr].push(j);
                             break;
                         }
                     }
-                    if(urlDict[url1].length != 2){
-                        console.log("error "+url1+": cannot find ending time");
+                    if(urlDict[urlOnStartArr].length != 2){
+                        console.log("error "+urlOnStartArr+": cannot find ending time");
                     }
                 }
 
@@ -115,32 +160,52 @@ var contentScriptListener = function(message, sender, sendResponse) {
                  * endTime and there is no request C whose endTime is in the interval of (B's endTime, A's startTime)            
                  */
                 var degree = 0;
-                var curEndTime = sortedRequestsOnStartTime[0].endTime;
-                sortedRequestsOnStartTime[0].degree = 0;
+                var stdStartTime = sortedRequestsOnStartTime[0].startTime;
+                var stdEndTime = sortedRequestsOnEndTime[0].endTime;
+                console.log(sortedRequestsOnStartTime[0].url.toString()+" <==> " + 
+                            sortedRequestsOnEndTime[0].url.toString());
+                sortedRequestsOnEndTime[0].degree = 0;
                 var j = 0;
+
                 for(var i in sortedRequestsOnStartTime){
-                    //delta = sortedRequestsOnStartTime[i].startTime -elements.requests[0].startTime;
-                    //console.log("SortedStartTime: "+delta+" "+sortedRequestsOnStartTime[i].url);
+                    delta = sortedRequestsOnStartTime[i].startTime -elements.requests[0].startTime;
+                    console.log("SortedStartTime: "+delta + " " + 
+                                sortedRequestsOnStartTime[i].url.toString()+
+                                " "+sortedRequestsOnStartTime.length);
                     if(i==0)
                         continue;
                     var startTime = sortedRequestsOnStartTime[i].startTime;
-                    for( ; j<sortedRequestsOnEndTime.length - 1; j++){
+                    if(startTime <= stdEndTime){
+                        console.log("ignore req: "+i+" "+sortedRequestsOnStartTime[i].url.toString());
+                        console.log("stdStartTime:"+stdStartTime+" stdEndTime:"+stdEndTime+
+                                    " startTime:"+startTime);
+                        continue;
+                    }
+                    
+                    while(j<sortedRequestsOnEndTime.length - 1){
                         if( (startTime>=sortedRequestsOnEndTime[j].endTime) && 
                             (startTime<sortedRequestsOnEndTime[j+1].endTime)){
-                            url = sortedRequestsOnEndTime[i].url;
-                            item = urlDict[url];
-                            sortedRequestsOnEndTime[item[1]].degree = sortedRequestsOnEndTime[j].degree + 1;
+                            var tmpURL = sortedRequestsOnEndTime[i].url.url;
+                            item = urlDict[tmpURL];
+                            sortedRequestsOnEndTime[item[1]].degree = 
+                                        sortedRequestsOnEndTime[j].degree + 1;
+                            break;
                             //degree = degree < sortedRequestsOnEndTime[item[1]].degree ?  
                             //            sortedRequestsOnEndTime[item[1]].degree : degree;
                         }
+                        else{
+                            j++;
+                        }
+                        console.log(startTime);
                     }
                 }
                 for(var i in sortedRequestsOnEndTime){
                     var delta1 = sortedRequestsOnEndTime[i].startTime -elements.requests[0].startTime;
                     var delta2 = sortedRequestsOnEndTime[i].endTime -elements.requests[0].startTime;
                     var degree = sortedRequestsOnEndTime[i].degree;
-                    url = sortedRequestsOnEndTime[i].url;
+                    var tmpURL = sortedRequestsOnEndTime[i].url.url;
                     console.log("degree: "+degree+" from:"+delta1+" to:"+delta2+" url:"+url);
+                    //console.log("degree: "+degree+" from:"+delta1+" to:"+delta2);
                 }
                 
               elements = new URLElements();
